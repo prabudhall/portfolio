22e310a0024140b999668f98571dddf6947e0270
Prabudh Kumar <prabudh2002@gmail.com>
2023-02-26 14:33:58 +0530

    Added email sending feature in contact us box

---
+++ index.js
@@ -0,0 +1,31 @@
+function handle(){
+  document.querySelector(".subbtn button").innerHTML = "Submitting";
+  var name = document.getElementById("name").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+  var params = {
+    name: name,
+    subject: subject,
+    message: message
+  };
+  if(!name || !subject || !message){
+    document.querySelector(".subbtn button").innerHTML = "Submit";
+    alert("Fields cannot be empty");
+    return;
+  }
+  // alert("Submitting the data");
+
+  emailjs.send(process.env.SERVICE, process.env.TEMPLATE, params)
+    .then(function(response) {
+      document.getElementById("name").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      document.querySelector(".subbtn button").innerHTML = "Submit";
+      console.log('SUCCESS!', response.status, response.text);
+      console.log(params);
+      alert("Message sent successfully");
+    }, function(error) {
+       console.log('FAILED...', error);
+       alert("Error in sending message");
+    });
+};
